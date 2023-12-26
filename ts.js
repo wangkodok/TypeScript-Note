f56e8944ecce66b2b4877d32f6efb0dd8a64156f
wangkodok <wangkodok@gmail.com>
2023-12-26 18:12:15 +0900

    타입스크립트 문법 작성

--- ts.js
+++ ts.js
@@ -1,1 +1,21 @@
 var strTest = "ok";
+var objArrType;
+// objArrType = {
+//   name: "string",
+//   age: 100,
+// };
+// 객체배열
+objArrType = [
+  {
+    name: "string",
+    age: 100,
+  },
+];
+// let objArrType: {
+//   name: string;
+//   age: number;
+// }[];
+function generics(name) {
+  return name + "string";
+}
+generics("type");
--- ts.ts
+++ ts.ts
@@ -1,1 +1,33 @@
 const strTest: string = "ok";
+
+let objArrType: {
+  name: string;
+  age: number;
+}[];
+
+// objArrType = {
+//   name: "string",
+//   age: 100,
+// };
+
+// 객체배열
+objArrType = [
+  {
+    name: "string",
+    age: 100,
+  },
+];
+
+// let objArrType: {
+//   name: string;
+//   age: number;
+// }[];
+
+// 제네릭 함수
+function generics<Type>(name: Type): string {
+  return name + "string";
+}
+
+generics<string>("type");
+generics<number>(100);
+generics<boolean>(true);
