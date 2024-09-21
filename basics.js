bf219b3c282da07a88594558c36729a4889395c4
wangkodok <wangkodok@gmail.com>
2024-09-21 19:35:33 +0900

    타입스크립트 제네릭 함수

--- basics.js
+++ basics.js
@@ -7,7 +7,20 @@ var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
     }
     return to.concat(ar || Array.prototype.slice.call(from));
 };
+// 1
 function fun(a, b) {
     return __spreadArray(__spreadArray([], a, true), [b], false);
 }
-var results = fun([1, 2], 2);
+var results = fun([1, 2], 3); // [1, 2, 3]
+// 2
+function genericsFun(a, b) {
+    return __spreadArray(__spreadArray([], a, true), [b], false);
+}
+var genericsResults = genericsFun(["1", 2], 3); // ["1", 2, 3]
+console.log(genericsResults, "인자 타입 다르게 지정");
+// 3
+function genericsFunction(result) {
+    return result + "함수";
+}
+var genericsResult = genericsFunction("제네릭");
+console.log(genericsResult, "값 확인");
--- basics.ts
+++ basics.ts
@@ -1,5 +1,22 @@
+// 1
 function fun<Type>(a: Type[], b: Type) {
   return [...a, b];
 }
 
-const results = fun([1, 2], "3"); // [1, 2, "3"]
+const results = fun([1, 2], 3); // [1, 2, 3]
+
+// 2
+function genericsFun<T>(a: T[], b: number) {
+  return [...a, b];
+}
+
+const genericsResults = genericsFun(["1", 2], 3); // ["1", 2, 3]
+console.log(genericsResults, "인자 타입 다르게 지정");
+
+// 3
+function genericsFunction<T>(result: T) {
+  return result + "함수";
+}
+
+const genericsResult = genericsFunction("제네릭");
+console.log(genericsResult, "값 확인");
